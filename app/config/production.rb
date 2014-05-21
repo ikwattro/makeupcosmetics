a0904fda77ee425be953fd9ef6cbcfc4395eca1b
Christophe Willemsen <willemsen.christophe@gmail.com>
2014-05-22 01:14:20 +0200

    new params

--- app/config/production.rb
+++ app/config/production.rb
@@ -1,7 +1,40 @@
-server '46.226.109.127', :app, :web, :primary => true
 set :domain,      "46.226.109.127"
 set :deploy_to,   "/srv/datadisk01/web/cosmetics/site/deploys"
+set :app_path,    "app"
+
+set :repository,  "git@github.com:kwattro/makeupcosmetics.git"
+set :scm,         :git
+# Or: `accurev`, `bzr`, `cvs`, `darcs`, `subversion`, `mercurial`, `perforce`, or `none`
+
+set :model_manager, "doctrine"
+# Or: `propel`
+
+role :web,        domain                         # Your HTTP server, Apache/etc
+role :app,        domain, :primary => true       # This may be the same as your `Web` server
+
+set  :keep_releases,  3
 set  :use_sudo, true
 set  :user, "angusyoung"
 set  :ssh_options, { :forward_agent => true }
 default_run_options[:pty] = true
+
+#Symfony2 Config
+set :shared_files,      ["app/config/parameters.yml"]
+set :shared_children,     [app_path + "/logs", web_path + "/uploads", "vendor"]
+set :use_composer, true
+set :update_vendors, true
+
+# Be more verbose by uncommenting the following line
+logger.level = Logger::MAX_LEVEL
+
+task :upload_parameters do
+  origin_file = "app/config/parameters/parameters_prod.yml"
+  destination_file = shared_path + "/app/config/parameters.yml" # Notice the
+  shared_path
+
+  try_sudo "mkdir -p #{File.dirname(destination_file)}"
+  top.upload(origin_file, destination_file)
+end
+
+after "symfony:composer:install", "upload_parameters"
+
--- app/config/staging.rb
+++ app/config/staging.rb
@@ -1,1 +1,29 @@
-server '192.168.56.10', :app, :web, :primary => true
+set :domain,      "192.168.56.10"
+set :deploy_to,   "/home/ikwattro/www/sites/deploys"
+set :app_path,    "app"
+
+
+set :repository,  "https://github.com/kwattro/makeupcosmetics.git"
+set :scm,         :git
+# Or: `accurev`, `bzr`, `cvs`, `darcs`, `subversion`, `mercurial`, `perforce`, or `none`
+
+set :model_manager, "doctrine"
+# Or: `propel`
+
+role :web,        domain                         # Your HTTP server, Apache/etc
+role :app,        domain, :primary => true       # This may be the same as your `Web` server
+
+set  :keep_releases,  3
+set  :use_sudo, true
+set  :user, "ikwattro"
+set  :ssh_options, { :forward_agent => true }
+default_run_options[:pty] = true
+
+#Symfony2 Config
+set :shared_files,      ["app/config/parameters.yml"]
+set :shared_children,     [app_path + "/logs", web_path + "/uploads", "vendor"]
+set :use_composer, true
+set :update_vendors, true
+
+# Be more verbose by uncommenting the following line
+logger.level = Logger::MAX_LEVEL

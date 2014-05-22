a4452d9e8cec6c4b5f03c0678a2ec10fe09ff622
Christophe Willemsen <willemsen.christophe@gmail.com>
2014-05-22 17:10:08 +0200

    added analytics

--- app/config/production.rb
+++ app/config/production.rb
@@ -1,6 +1,7 @@
 set :domain,      "217.70.189.241"
 set :deploy_to,   "/srv/webdisk/web/www/makeupcosmetics/"
 set :app_path,    "app"
+set :cache_path,          app_path + "/cache"
 
 set :repository,  "git@github.com:kwattro/makeupcosmetics.git"
 set :scm,         :git
@@ -21,7 +22,7 @@ default_run_options[:pty] = true
 
 #Symfony2 Config
 set :shared_files,      ["app/config/parameters.yml"]
-set :shared_children,     [app_path + "/logs", web_path + "/uploads", "vendor", "/media"]
+set :shared_children,     [app_path + "/logs", web_path + "/uploads", "vendor", web_path + "/media"]
 set :use_composer, true
 set :update_vendors, true
 
@@ -29,14 +30,32 @@ set :update_vendors, true
 logger.level = Logger::MAX_LEVEL
 
 task :upload_parameters do
-  origin_file = "app/config/parameters/parameters_prod.yml"
+  origin_file = "app/config/parameters/parameters_staging.yml"
   destination_file = shared_path + "/app/config/parameters.yml" # Notice the
   shared_path
 
   try_sudo "mkdir -p #{File.dirname(destination_file)}"
   try_sudo "chown -R angusyoung:www-data #{shared_path}"
+  #try_sudo "mkdir -p #{app_path}/cache"
+  #try_sudo "chown -R angusyoung:www-data #{app_path}/cache"
+  try_sudo "chmod -R g+w #{latest_release}/#{cache_path}"
   top.upload(origin_file, destination_file)
 end
+
+task :set_correct_cache do
+    try_sudo "chown -R angusyoung:www-data #{latest_release}/#{cache_path}"
+    try_sudo "chmod -R g+w #{latest_release}/#{cache_path}"
+end
+
+task :set_media_writable do
+    try_sudo "chown -R angusyoung:www-data #{shared_path}/web/uploads"
+    try_sudo "chown -R angusyoung:www-data #{shared_path}/web/media"
+    try_sudo "chmod -R g+w #{shared_path}/web/uploads"
+    try_sudo "chmod -R g+w #{shared_path}/web/media"
+end
+
+before "deploy:restart", "set_correct_cache"
+before "deploy:restart", "set_media_writable"
 
 
 
@@ -56,6 +75,7 @@ namespace :composer do
 end
 
 before :deploy, "deploy:copy_database_config"
+#before "deploy:restart", "deploy:set_permissions"
 
 namespace :deploy do
 task :copy_database_config do
@@ -64,8 +84,7 @@ task :copy_database_config do
 end
 
 
-set :writable_dirs,       ["app/cache", "app/logs", "web/media", "web/uploads"]
+set :writable_dirs,       ["app/cache", "app/logs"]
 set :webserver_user,      "angusyoung"
 set :permission_method,   :chown
 set :use_set_permissions, true
-

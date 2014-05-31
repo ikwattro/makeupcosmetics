1fc076239efa6659a527c34aa42387592359d006
Christophe Willemsen <willemsen.christophe@gmail.com>
2014-05-31 11:38:46 +0200

    added fm promo

--- src/Store/MarketingBundle/Controller/CampaignController.php
+++ src/Store/MarketingBundle/Controller/CampaignController.php
@@ -23,13 +23,26 @@ class CampaignController extends Controller
         );
     }
 
+    /**
+     * @Route("/campaign/email/promoFm", name="campaign_promoFm")
+     * @Template()
+     */
+    public function promoFmAction()
+    {
+        //var_dump($this->generateUrl('email_followback'));
+        return array(
+            'followback_url' => $this->generateUrl('email_followback'),
+            'email' => 'willemsen.christophe@gmail.com'
+        );
+    }
+
     private function sendCampaignEmail($followbackUrl, $email)
     {
         $message = \Swift_Message::newInstance()
             ->setSubject('MakeUp Cosmetics de Claude Haest')
             ->setFrom(array('makeupcosmetics.eu@gmail.com' => 'MakeUp Cosmetics.eu'))
             ->setTo($email)
-            ->setBody($this->renderView('StoreMarketingBundle:Campaign:newStart.html.twig', array(
+            ->setBody($this->renderView('StoreMarketingBundle:Campaign:promoFm.html.twig', array(
                 'followback_url' => $followbackUrl,
                 'email' => $email
             )), 'text/html')
@@ -63,10 +76,38 @@ class CampaignController extends Controller
 
     }
 
+    /**
+     * @Route("/admin/campaign/email/send/promoFm", name="campaign_send_promoFm")
+     * @Template()
+     */
+    public function sendPromoFmAction()
+    {
+        $targets = array();
+
+        $em = $this->getDoctrine()->getManager();
+        $entities = $em->getRepository('StoreMarketingBundle:TargetEmail')->findAll();
+        foreach ($entities as $target) {
+            if ($this->isValidForFrench($target)) {
+                //$this->sendCampaignEmail($this->generateUrl('email_followback'), strtolower($target->getEmail()));
+                $targets[] = $target->getEmail();
+            }
+        }
+
+        return array(
+            'count' => count($targets),
+            'targets' => $targets
+        );
+
+    }
+
     public function isValidForFrench($target)
     {
         if ($target->getLanguage() == 'fr') {
             return true;
+        }
+
+        if (null !== $target->getLanguage() && ($target->getLanguage() =='nl' || $target->getLanguage() == 'de')) {
+            return false;
         }
 
         $email = $target->getEmail();
